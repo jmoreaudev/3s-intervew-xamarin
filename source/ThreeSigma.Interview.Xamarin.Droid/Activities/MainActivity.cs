704dc34f6a59a374ae274183813a7e02405e1c0a
Jonathan Moreau <jon.moreau@3sigmasoftware.com>
2020-03-27 12:08:09 -0500

    Added Android and iOS projects to solution

---
+++ source/ThreeSigma.Interview.Xamarin.Droid/Activities/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using ThreeSigma.Interview.Xamarin.Shared.ViewModels;
+
+namespace ThreeSigma.Interview.Xamarin.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private readonly MainViewModel viewModel;
+
+        public MainActivity()
+        {
+            viewModel = new MainViewModel("Hello world!");
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Title = "Home";
+            SetContentView(Resource.Layout.activity_main);
+            
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
---
+++ source/ThreeSigma.Interview.Xamarin.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeSigma.Interview.Xamarin.Shared.ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly string message;
+
+        public MainViewModel(string message)
+        {
+            this.message = message;
+        }
+
+        public string Message => message;
+    }
+}
